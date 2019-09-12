758443893bff615fb9217355b99a611edd30da68
Paco-Mancilla <franciscojavier_mancilla@ucol.mx>
2019-09-12 07:30:42 -0500

    first commit

---
+++ tipografia.js
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-4 bg-danger">
+                <p>LogoEmpresa</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-8 bg-warning">
+                <p>Menu de navegacion</p>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-sm-12 bg-secondary align-self-center p-5">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, ducimus eveniet quam recusandae facilis necessitatibus. Cumque accusantium qui nisi commodi harum dolorum, laudantium autem consectetur repellendus, optio quam unde architecto.</p>
+            </div>
+        </div>
+          <div class="row ">
+            <div class="col-xs-12 col-sm-6 col-md-4 bg-success">
+                <h1>Titulo 1</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi deleniti placeat tenetur cumque officiis excepturi dolor neque, explicabo inventore, sequi ab enim dolorum dignissimos consequuntur, nostrum doloribus expedita error perferendis?</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-4 bg-info">
+                    <h1>Titulo 1</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi deleniti placeat tenetur cumque officiis excepturi dolor neque, explicabo inventore, sequi ab enim dolorum dignissimos consequuntur, nostrum doloribus expedita error perferendis?</p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 bg-success">
+                    <h1>Titulo 1</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi deleniti placeat tenetur cumque officiis excepturi dolor neque, explicabo inventore, sequi ab enim dolorum dignissimos consequuntur, nostrum doloribus expedita error perferendis?</p>
+            </div>
+          </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
